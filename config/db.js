a6724d255d40f918374d34de13452100b9ca5e6c
林弘季 <hayashihiroki@hayashihiroshikinoMacBook-Air.local>
2025-03-08 16:16:57 +0900

    MongoDB Atlas で接続するように db.js を変更

--- config/db.js
+++ config/db.js
@@ -3,9 +3,20 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = 'mongodb://localhost:27017/yourDatabaseName'; // 必要に応じて変更
+    // MongoDB Atlasの接続文字列
+    const mongoURI = 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/<yourDatabaseName>?retryWrites=true&w=majority';
 
-    await mongoose.connect(mongoURI);
+    // サーバーレス環境向けの接続設定
+    if (mongoose.connection.readyState >= 1) {
+      console.log('既にMongoDBに接続されています');
+      return;
+    }
+
+    // MongoDBに接続
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log('MongoDBに接続しました');
   } catch (err) {
